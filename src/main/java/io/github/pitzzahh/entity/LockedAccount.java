152940262c168b057296ed650c6599f80affbc00
pitzzahh <araopj2191@gmail.com>
2022-07-27 22:39:31 +0800

    add record for locked accounts

---
+++ src/main/java/io/github/pitzzahh/entity/LockedAccount.java
@@ -0,0 +1,19 @@
+package io.github.pitzzahh.entity;
+
+import com.github.pitzzahh.utilities.classes.enums.Gender;
+import static com.github.pitzzahh.utilities.classes.TextColors.*;
+
+/**
+ * Class used for making locked accounts.
+ * @param accountNumber the account number of a locked account.
+ * @param name the name of the person with the locked account.
+ * @param gender the gender of the person.
+ */
+public record LockedAccount(String accountNumber, String name, Gender gender) {
+    @Override
+    public String toString() {
+        return "ACCOUNT NUMBER: " + BLUE_BOLD_BRIGHT   + this.accountNumber() + "\n" +
+               "NAME          : " + PURPLE_BOLD_BRIGHT + this.name()          + "\n" +
+               "GENDER        : " + YELLOW_BOLD_BRIGHT + this.gender().name() + "\n" + RESET;
+    }
+}
