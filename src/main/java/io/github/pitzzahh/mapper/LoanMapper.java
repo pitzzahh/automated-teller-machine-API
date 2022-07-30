1d51a127f970ab7e697e02374c8cea5da56a96fb
pitzzahh <araopj2191@gmail.com>
2022-07-30 18:23:52 +0800

    add LoanMapper for mapping loans from the database

---
+++ src/main/java/io/github/pitzzahh/mapper/LoanMapper.java
@@ -0,0 +1,28 @@
+package io.github.pitzzahh.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import io.github.pitzzahh.entity.Loan;
+import org.springframework.jdbc.core.RowMapper;
+import com.github.pitzzahh.utilities.SecurityUtil;
+
+public class LoanMapper implements RowMapper<Loan> {
+
+    /**
+     * maps the data from the table to a {@code Loan} object.
+     * @param resultSet the ResultSet to map (pre-initialized for the current row)
+     * @param numberOfRow the number of the current row
+     * @return {@code Loan} object.
+     * @throws SQLException if something is went wrong.
+     */
+    @Override
+    public Loan mapRow(ResultSet resultSet, int numberOfRow) throws SQLException {
+        return new Loan(
+                Integer.parseInt(SecurityUtil.decrypt(resultSet.getString("loan_number"))),
+                SecurityUtil.decrypt(resultSet.getString("account_number")),
+                resultSet.getDate("date_of_loan").toLocalDate(),
+                Double.parseDouble(SecurityUtil.decrypt(resultSet.getString("amount"))),
+                Integer.parseInt(SecurityUtil.decrypt(resultSet.getString("loan_count")))
+        );
+    }
+}
