91d41f7b2e8484c49ae0a3b078153d4fd629ac9f
pitzzahh <araopj2191@gmail.com>
2022-08-02 22:26:29 +0800

    changed type to a class. create methods that allow any program to configure the connection to the database.

--- src/main/java/io/github/pitzzahh/database/DatabaseConnection.java
+++ src/main/java/io/github/pitzzahh/database/DatabaseConnection.java
@@ -1,24 +1,50 @@
 package io.github.pitzzahh.database;
 
+import java.util.stream.Stream;
 import io.github.pitzzahh.service.AtmService;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
  * Interface used by services to gain access to the database.
  */
-public interface DatabaseConnection {
+public class DatabaseConnection {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    public DatabaseConnection setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+        return this;
+    }
+
+    public DatabaseConnection setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public DatabaseConnection setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public DatabaseConnection setPassword(String password) {
+        this.password = password;
+        return this;
+    }
 
     /**
-     * Set up the datasource.
-     * @see AtmService
+     * Returns the complete data source.
      * @return the datasource.
+     * @see AtmService
      */
-    static DriverManagerDataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost/atm");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("!Password123");
+    public DriverManagerDataSource getDataSource() {
+        var dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(this.driverClassName);
+        dataSource.setUrl(this.url);
+        dataSource.setUsername(this.username);
+        dataSource.setPassword(this.password);
         return dataSource;
     }
 
