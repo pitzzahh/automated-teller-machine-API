5a51c36f26a8cc9ed60ec05c934efa031490de7b
pitzzahh <araopj2191@gmail.com>
2022-08-04 22:51:52 +0800

    added message colors

--- src/main/java/io/github/pitzzahh/entity/Message.java
+++ src/main/java/io/github/pitzzahh/entity/Message.java
@@ -8,14 +8,17 @@ import java.text.NumberFormat;
 import java.time.LocalDateTime;
 import java.time.format.FormatStyle;
 import java.time.format.DateTimeFormatter;
+import static com.github.pitzzahh.utilities.classes.TextColors.*;
 
 /**
  * record used to generate message.
- * @param loan the loan loan.
- * @param client the client
+ * @param loan the loan request.
+ * @param client the client who made the loan.
  */
 public record Message(Loan loan, Client client) {
+
     private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance();
+
     @Override
     public String toString() {
 
@@ -23,18 +26,18 @@ public record Message(Loan loan, Client client) {
         cal.setTime(Date.valueOf(loan.dateOfLoan()));
         cal.add(Calendar.MONTH, 2);
 
-        return String.format(
-                "Dear %s,                                                                                " + "\n\n" +
-                "    We have received your loan request of %s, on %s."                                     + "\n"   +
-                "The Loan amount has been added to your savings. You must pay it before %s, "              + "\n"   +
-                "or else there will be problems that may occur in your account."                           + "\n"   +
+        return String.format(CYAN_BOLD +
+                "Dear %s,                                                                                " + "\n\n" + CYAN_BOLD +
+                "    We have received your loan request of %s, on %s."                                     + "\n"   + CYAN_BOLD +
+                "The Loan amount has been added to your savings. You must pay it before %s, "              + "\n"   + CYAN_BOLD +
+                "or else there will be problems that may occur in your account."                           + "\n"   + CYAN_BOLD +
                 "\n\n" +
-                "                                                                      Sincerly          " + "\n"   +
-                "                                                                      Admin             ",
-                client.details().getFirstName().concat(" " + client.details().getLastName()),
-                Currency.getInstance("PHP").getSymbol().concat(NUMBER_FORMAT.format(loan.amount())),
-                loan.dateOfLoan().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)),
-                LocalDateTime.ofInstant(cal.getTime().toInstant(), ZoneId.of("GMT")).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))
+                "                                                                      Sincerly          " + "\n"   + RED_BOLD_BRIGHT +
+                "                                                                      Admin             " + RESET,
+                YELLOW_BOLD + client.details().getFirstName().concat(" " + client.details().getLastName()) + RESET,
+                PURPLE_BOLD + Currency.getInstance("PHP").getSymbol().concat(NUMBER_FORMAT.format(loan.amount())) + RESET,
+                BLUE_BOLD + loan.dateOfLoan().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))  +RESET,
+                RED_BOLD_BRIGHT + LocalDateTime.ofInstant(cal.getTime().toInstant(), ZoneId.of("GMT")).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)) + RESET
         );
     }
 }
