910487f3af2cef6de1d106ed4e4731a8bf2eabc9
pitzzahh <araopj2191@gmail.com>
2022-08-08 00:36:32 +0800

    added is_declined column

--- src/main/resources/loans.sql
+++ src/main/resources/loans.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS loans (
     account_number TEXT NOT NULL,
     date_of_loan DATE NOT NULL,
     amount TEXT NOT NULL,
-    pending TEXT NOT NULL
+    pending TEXT NOT NULL,
+    is_declined TEXT NOT NULL
 );
