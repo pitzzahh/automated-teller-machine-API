e432a9b9d252eec7772ba58baf693ac0a8eddd66
pitzzahh <araopj2191@gmail.com>
2022-08-10 22:49:37 +0800

    added @param tag

--- src/main/java/io/github/pitzzahh/atm/entity/Loan.java
+++ src/main/java/io/github/pitzzahh/atm/entity/Loan.java
@@ -25,7 +25,7 @@ public record Loan(int loanNumber, String accountNumber, LocalDate dateOfLoan, d
      * @param accountNumber the account number that made the loan.
      * @param dateOfLoan the date when the loan is processed.
      * @param amount the amount that was loan.
-     * @param loanCount the count on how many times the account made a loan.
+     * @param pending if the loan is not yet resolved.
      */
     public Loan(String accountNumber, LocalDate dateOfLoan, double amount, boolean pending) {
         this(0, accountNumber, dateOfLoan, amount, pending, false);
