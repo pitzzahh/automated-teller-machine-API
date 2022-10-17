f03a07b74e233a426714682fffef3bf4c1fb8e64
pitzzahh <araopj2191@gmail.com>
2022-10-17 22:27:54 +0800

    moved to same folder dao

--- src/test/java/io/github/pitzzahh/atm/dao/InDatabaseAtmServiceTest.java
+++ src/test/java/io/github/pitzzahh/atm/dao/InDatabaseAtmServiceTest.java
@@ -1,4 +1,4 @@
-package io.github.pitzzahh.service;
+package io.github.pitzzahh.atm.dao;
 
 import java.time.*;
 import java.util.Map;
--- src/test/java/io/github/pitzzahh/atm/dao/InMemoryAtmServiceTest.java
+++ src/test/java/io/github/pitzzahh/atm/dao/InMemoryAtmServiceTest.java
@@ -1,11 +1,11 @@
-package io.github.pitzzahh.atm.database;
+package io.github.pitzzahh.atm.dao;
 
+import io.github.pitzzahh.atm.dao.InMemory;
 import io.github.pitzzahh.atm.service.AtmService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class InMemoryAtmServiceTest {
