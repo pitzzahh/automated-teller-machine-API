27f5608f30f3e06e59a87f754c06250aad64aec9
pitzzahh <pitzzahh@gmail.com>
2022-10-28 22:46:50 +0800

    added module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,17 @@
+/**
+ * Module info
+ */
 module automated.teller.machine.API {
-    requires spring.jdbc;
     requires util.classes;
+    requires spring.jdbc;
     requires java.sql;
+
+    exports io.github.pitzzahh.atm.dao;
+    exports io.github.pitzzahh.atm.database;
+    exports io.github.pitzzahh.atm.entity;
+    exports io.github.pitzzahh.atm.exceptions;
+    exports io.github.pitzzahh.atm.mapper;
+    exports io.github.pitzzahh.atm.service;
+    exports io.github.pitzzahh.atm.validator;
 
 }
